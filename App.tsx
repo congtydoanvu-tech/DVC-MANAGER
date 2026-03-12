25a6139379aed7e7d22486ad58785dd4aa64d8ad
congtydoanvu-tech <congtydoanvu@gmail.com>
2026-03-12 23:22:24 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -95,3 +95,14 @@ const App: React.FC = () => {
 };
 
 export default App;
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
