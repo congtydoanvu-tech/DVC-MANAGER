78d4b1a633f1698689257814327c9b441df99e6d
congtydoanvu-tech <congtydoanvu@gmail.com>
2026-03-12 23:48:08 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      strict: false // Cho phép Vite đọc file ở thư mục gốc
-    }
+  build: {
+    outDir: 'dist'
   }
 })
